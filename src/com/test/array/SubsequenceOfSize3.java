75862b50fcd7c8a8bfd4f8d445d70193c0d32a06
sumit_danish <sumit.polite@gmail.com>
2017-08-31 00:56:45 +0530

    Commiting

---
+++ src/com/test/array/SubsequenceOfSize3.java
@@ -0,0 +1,70 @@
+package com.test.array;
+
+public class SubsequenceOfSize3 {
+    public static void main(String[] args) {
+        int[] a = {4,3,2,1};
+        smallest(a);
+    }
+    private static void subsequence(int[] a){
+        int[] smallestLeft = new int[a.length];
+        int[] rightGreatest = new int[a.length];
+        int min = 0;
+        int max = a.length-1;
+        smallestLeft[0] = -1;
+        rightGreatest[a.length-1] = -1;
+        for(int i = 0;i<a.length;i++){
+            if(a[i] <= a[min]){
+                min = i;
+                smallestLeft[i] = -1;
+            }else {
+                smallestLeft[i] = min;
+            }
+        }
+        for(int i = a.length-2;i>=0;i--){
+            if(a[max] <= a[i]){
+                max = i;
+                rightGreatest[i] = -1;
+            }else {
+                rightGreatest[i] = max;
+            }
+        }
+
+        for(int i =0;i<a.length;i++){
+            if(smallestLeft[i] != -1 && rightGreatest[i] != -1){
+                System.out.println(a[smallestLeft[i]]+" : "+a[i]+" : "+a[rightGreatest[i]]);
+                break;
+            }
+        }
+    }
+
+    private static void smallest(int[] a){
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int ind = 0;
+        int i = 0;
+        for(i = 0; i+1 < a.length;i++){
+            if(a[i] < a[i+1]){
+                max1 = a[i];
+                max2 = a[i+1];
+                ind = i;
+                break;
+            }
+        }
+        if(i == a.length-1){
+            System.out.println("No Subsequence exist !!!");
+            return;
+        }
+        int third = 0;
+        while(ind < a.length){
+            if(a[ind] > max1 && a[ind] > max2){
+                third = a[ind];
+                break;
+            }
+            ind++;
+        }
+
+        System.out.println(max1+" : "+max2+": "+third);
+
+    }
+}
+
